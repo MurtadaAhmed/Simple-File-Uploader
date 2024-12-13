0d8a63ae8b55f7d95b179299e5860c172f02c024
Murtada Ahmed <murtadhabg@gmail.com>
2024-12-13 10:00:35 +0200

    add mailer script

---
+++ scripts/mailer.php
@@ -0,0 +1,10 @@
+<?php
+
+function send_verification_email($email, $verification_code) {
+    $subject = "Verify your email";
+    $message = "Click the link below to verify your email: \n";
+    $domain = $_SERVER['HTTP_HOST'];
+    $message .= "http://$domain/verify.php?code=" . $verification_code;
+    $headers = "From: noreply@$domain";
+    mail($email, $subject, $message, $headers);
+}
