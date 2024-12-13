82877add3ee38200b60b49ae1211e99a8ea95525
Murtada Ahmed <murtadhabg@gmail.com>
2024-12-13 10:49:42 +0200

    update login

--- templates/login.php
+++ templates/login.php
@@ -1,6 +1,6 @@
 <?php
 
-require 'scripts/auth.php';
+require '../scripts/auth.php';
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $username = $_POST['username'];
@@ -14,17 +14,13 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 }
 ?>
 
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Login</title>
-</head>
-<body>
+<?php include 'header.php'; ?>
+<main>
 <h1>Login</h1>
 <form method="POST">
     <input type="text" name="username" placeholder="Username" required>
     <input type="password" name="password" placeholder="Password" required>
     <button type="submit">Login</button>
 </form>
-</body>
-</html>
+</main>
+<?php include 'footer.php'; ?>
