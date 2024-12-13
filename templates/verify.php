40662424a447085e2c41c96e16440e9c339b0212
Murtada Ahmed <murtadhabg@gmail.com>
2024-12-13 18:11:38 +0200

    update verify

--- templates/verify.php
+++ templates/verify.php
@@ -6,13 +6,12 @@ global $conn;
 
 if (isset($_GET['code'])) {
     $code = $_GET['code'];
-    $stmt = $conn->prepare("UPDATE users SET emai_verified=1 WHERE verification_code = ? ");
-    $stmt->bind_param("s", $code);
-    if ($stmt->execute() && $stmt->affected_rows > 0) {
-        echo "Email verified successfully! <a href='login.php'>Login here</a>";
+    if (verify_email($code)) {
+        echo "Email verified successfully!";
     } else {
         echo "Invalid or expired verification code.";
     }
+
 }
 
 //if (isset($_GET['code'])) {
@@ -25,4 +24,3 @@ if (isset($_GET['code'])) {
 //        echo "Invalid or expired verification code.";
 //    }
 //}
-?>
