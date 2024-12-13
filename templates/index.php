9dc138f1d42f177571b0e8579079a0641bfa667a
Murtada Ahmed <murtadhabg@gmail.com>
2024-12-13 10:41:05 +0200

    add index

---
+++ templates/index.php
@@ -0,0 +1,8 @@
+<?php include 'header.php'; ?>
+
+<main>
+    <h2>Welcome</h2>
+    <p><a href="register.php">Register</a> or <a href="login.php">Login</a> to start uploading files. </p>
+</main>
+
+<?php include 'footer.php'; ?>
