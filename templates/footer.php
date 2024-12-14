4a44da84b6073051c25bf2c1c2b75308b176a0e5
Murtada Ahmed <murtadhabg@gmail.com>
2024-12-14 11:57:54 +0200

    added bootstrap

--- templates/footer.php
+++ templates/footer.php
@@ -1,4 +1,5 @@
 <footer>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"></script>
     <p>&copy; <?php echo date('Y');?> Simple File Uploader</p>
 </footer>
 </body>
