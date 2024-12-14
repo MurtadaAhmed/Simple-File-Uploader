0d83fb4b8817b5122986c57a2bfba64cbd322bef
Murtada Ahmed <murtadhabg@gmail.com>
2024-12-14 11:12:14 +0200

    update list_files.php to return the url of the files as well

--- scripts/list_files.php
+++ scripts/list_files.php
@@ -15,19 +15,25 @@ $items_per_page = 5;
 $page = isset($_GET['page']) ? (int)$_GET['page'] : 1;
 $offset = ($page - 1) * $items_per_page;
 
-// Get the total count of files
+
 $stmt = $pdo->prepare("SELECT COUNT(*) FROM files WHERE user_id = ?");
 $stmt->execute([$user_id]);
 $total_files = $stmt->fetchColumn();
 $total_pages = ceil($total_files / $items_per_page);
 
-// Get the files for the current page
+
 $stmt = $pdo->prepare("SELECT * FROM files WHERE user_id = ? LIMIT ? OFFSET ?");
 $stmt->bindParam(1, $user_id, PDO::PARAM_INT);
 $stmt->bindParam(2, $items_per_page, PDO::PARAM_INT);
 $stmt->bindParam(3, $offset, PDO::PARAM_INT);
 $stmt->execute();
 $files = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+$base_url = '/uploads/' . $user_id;
+
+foreach ($files as &$file) {
+    $file['file_url'] = $base_url . '/' . $file['file_name'];
+}
 
 echo json_encode([
     'files' => $files,
